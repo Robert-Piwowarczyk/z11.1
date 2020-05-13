48e75d7015380c2219dec6555696c9f902187b63
HelloWorld <robpiw@op.pl>
2020-05-13 18:32:51 +0200

    Statystyki firmy

---
+++ src/EmployeeDataReader.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class EmployeeDataReader {
+    static Employee[] readFile(String fileName) throws FileNotFoundException {
+        final int linesNumber = countLines(fileName);
+        Employee[] employees = new Employee[linesNumber];
+        try (Scanner scanner = new Scanner(new File(fileName)) {
+           for(
+            int i = 0;
+            i<linesNumber;i++)
+
+            {
+                String csvLine = scanner.nextLine();
+                employees[i] = createEmployeeFromCsv(csvLine);
+            }
+               return employees;
+
+
+            private static Employee createEmployeeFromCsv(String csvLine) {
+                String[] word = csvLine.split(",");
+                String firstName = word[0];
+                String lastName = word[1];
+                int pesel = word[2];
+                String section = word[3];
+                double salary = word[4];
+                return new Employee(firstName, lastName, pesel, section, salary);
+            }
+
+            private static int countLines(String fileName) throws FileNotFoundException {
+                int lines = 0;
+                try (Scanner scanner = new Scanner(new File(fileName))) {
+                    while (scanner.hasNextLine()) {
+                        scanner.nextLine();
+                        lines++;
+                    }
+                }
+                return lines;
+            }
+        }
+             }
+}
+
+
+
+
